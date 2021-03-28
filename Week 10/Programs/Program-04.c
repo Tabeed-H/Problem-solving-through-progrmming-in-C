bda594479d99fdd652a101f5f3780bc35e8ea498
tabeed-h <tabeedhameed121@gmail.com>
2021-03-28 23:05:49 +0530

    program 4 added

---
+++ Week 10/Programs/Program-04.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void sort(int *a, int n);
+int main()
+{
+    int a[20];
+    int n,i; 
+    scanf("%d",&n); // Enter number of elements to sort is taken from test case data
+   
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]); // The elements of the array is taken from the test data
+    }
+
+sort(a, n); // Calling the sorting function
+
+    //Printing the sorted array 
+    for(i=0;i<n;i++)
+    {
+        printf("%d\n",a[i]);
+    }
+   return 0;
+}
+
+void sort(int *a , int n)
+{
+    for(int i = 0; i < n -1 ; i++)
+    {
+        for(int j = 0; j < n - 1; j++)
+        {
+            if(a[j] > a[j+1])
+            {
+                int temp = a[j+1];
+                a[j+1] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+}
